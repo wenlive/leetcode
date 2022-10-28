a7dd1a5b2988d358dc5e953c5abb5f48e4182cf3
李泊琦 <11137257@vivo.com>
2022-10-28 19:57:00 +0800

    add reverse without loop

--- leetcode/reverseLinkedList.go
+++ leetcode/reverseLinkedList.go
@@ -19,7 +19,8 @@ func (p *ListNode) Append(data int) {
 
 func NewLisNode() *ListNode {
 	var head *ListNode = new(ListNode)
-	for i := 0; i < 10; i++ {
+	head.Val = 1
+	for i := 2; i < 10; i++ {
 		head.Append(i)
 	}
 	return head
@@ -27,9 +28,35 @@ func NewLisNode() *ListNode {
 
 func Cout(head *ListNode) {
 	for head.Next != nil {
-		fmt.Println(head.Next.Val)
+		fmt.Println(head.Val)
 		head = head.Next
 	}
+	fmt.Println(head.Val)
+	fmt.Println("----------")
+}
+
+func Reverse(head *ListNode) *ListNode {
+	var behind *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = behind
+		behind = head
+		head = next
+	}
+	return behind
+}
+
+func ReverseWithoutLoop(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	//利用递归将head不停前移
+	last := ReverseWithoutLoop(head.Next)
+	//将后面的next指向当前位置
+	head.Next.Next = head
+	//清空原有正向连接
+	head.Next = nil
+	return last
 }
 
 func ReverseBetween(head *ListNode, m int, n int) *ListNode {
@@ -54,4 +81,32 @@ func ReverseBetween(head *ListNode, m int, n int) *ListNode {
 	return newHead.Next
 }
 
-//TODO reverse without for
+func ReverseBetweenV2(head *ListNode, m int, n int) *ListNode {
+	if head == nil || m >= n {
+		return head
+	}
+	newHead := &ListNode{Val: 0, Next: head}
+	pre := newHead
+	for count := 0; pre.Next != nil && count < m-1; count++ {
+		pre = pre.Next
+	}
+	if pre.Next == nil {
+		return head
+	}
+	cur := pre.Next
+	var behind *ListNode
+	for i := 0; i < n-m+1; i++ {
+		next := cur.Next
+		// if i == n-m {
+		// 	if next == nil {
+		// 		fmt.Println("touch the nil bound")
+		// 	}
+		// }
+		cur.Next = behind
+		behind = cur
+		cur = next
+	}
+	pre.Next.Next = cur
+	pre.Next = behind
+	return newHead.Next
+}
--- main.go
+++ main.go
@@ -10,10 +10,19 @@ func main() {
 	var nums = []int{1, -1, 0, 4}
 	fmt.Println(leetcode.ThreeSum(nums))
 
-	var a *leetcode.ListNode = leetcode.NewLisNode()
-	leetcode.Cout(a)
-	leetcode.ReverseBetween(a.Next, 2, 8)
-	leetcode.Cout(a)
-	fmt.Println("finished")
+	// var a *leetcode.ListNode = leetcode.NewLisNode()
+	// leetcode.Cout(a)
+	b := leetcode.Reverse(leetcode.NewLisNode())
+	leetcode.Cout(b)
+
+	b2 := leetcode.ReverseWithoutLoop(leetcode.NewLisNode())
+	leetcode.Cout(b2)
+
+	// c := leetcode.ReverseBetween(leetcode.NewLisNode(), 2, 8)
+	// leetcode.Cout(c)
+	// d := leetcode.ReverseBetweenV2(leetcode.NewLisNode(), 2, 8)
+	// leetcode.Cout(d)
+
+	// fmt.Println("finished")
 
 }
